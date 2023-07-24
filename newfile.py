46926304408d4ae07bd92a24ec214438d21ff17f
sudhabec <113691987+sudhabec@users.noreply.github.com>
2023-07-24 14:11:47 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,3 +1,4 @@
 This file contains python script....
 I am updating one line
 AWS certified cloud practitioner is intended for anyone.
+hello harika i am sudha speaking                                            
